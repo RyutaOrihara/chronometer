322c32b423d5186428a3e94cf6b00264971d5c5a
orihara <oriri75@gmail.com>
2018-12-06 08:14:20 +0900

    devise機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root 'events#index'
   resources :events do
     collection do
       post :confirm
     end
+  end
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
 end
