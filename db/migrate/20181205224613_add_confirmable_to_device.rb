8720f53ff2a8a2d50a3fd465c0ea45db089be2b9
orihara <oriri75@gmail.com>
2018-12-06 20:27:42 +0900

    herokuアップ

--- db/migrate/20181205224613_add_confirmable_to_device.rb
+++ db/migrate/20181205224613_add_confirmable_to_device.rb
@@ -3,7 +3,7 @@ class AddConfirmableToDevice < ActiveRecord::Migration[5.1]
     add_index :users, :confirmation_token, unique: true
     execute("UPDATE users SET confirmed_at = NOW()")
   end
-  
+
   def down
     remove_columns :users, :confirmation_token, :confirmed_at, :confirmation_sent_at
     remove_columns :users, :unconfirmed_email # Only if using reconfirmable
