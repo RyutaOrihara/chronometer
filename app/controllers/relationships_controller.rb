707139f3192da46f04698938072e5076e706ba2e
orihara <oriri75@gmail.com>
2018-12-16 13:44:02 +0900

    relation戻す

---
+++ app/controllers/relationships_controller.rb
@@ -0,0 +1,14 @@
+class RelationshipsController < ApplicationController
+  before_action :authenticate_user!
+  def create
+    @user = User.find(params[:relationship][:followed_id])
+    current_user.follow!(@user)
+    @user
+  end
+
+  def destroy
+    @user = Relationship.find(params[:id]).followed
+    current_user.unfollow!(@user)
+    @user
+  end
+end
