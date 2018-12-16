3c4c5f768d7baaa79b2101fc99398bbcd207e335
orihara <oriri75@gmail.com>
2018-12-16 14:41:27 +0900

    follow機能修正

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,12 +1,9 @@
 class RelationshipsController < ApplicationController
   before_action :authenticate_user!
+  respond_to? :js
   def create
     @user = User.find(params[:relationship][:followed_id])
     current_user.follow!(@user)
-<<<<<<< HEAD
-    @user
-=======
->>>>>>> relation
   end
 
   def destroy
